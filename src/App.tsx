11a0e291eb6bf198908300d6f24a4c09f887fbd1
Andrei Huryn <gurin.nord@gmail.com>
2024-03-22 10:06:50 +0300

    add collapsed to controlled accordion

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,24 @@ import {OnOff} from './components/OnOff/OnOff';
 import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRaiting/UncontrolledRaiting"
 import {Rating, RatingValueType} from "./components/Rating/Rating";
+import Accordion from "./components/Accordion/Accordion";
 
 function App(props: any) {
     console.log('App rendering');
 
     let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    console.log(ratingValue)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true)
 
     return (
         <div className={"App"}>
 
             <Rating value={ratingValue} onClick={setRatingValue}/>
+            <Accordion
+                titleValue={"Menu"}
+                collapsed={accordionCollapsed}
+                onClickAccordion={setAccordionCollapsed}
+            />
             {/*<PageTitle title = {'This is APP component'}/>*/}
             {/*<PageTitle title = {'My friends'}/>*/}
             {/*Article 1*/}
@@ -30,25 +38,21 @@ function App(props: any) {
             {/*<Rating value = {4}/>*/}
             {/*<Rating value = {5}/>*/}
 
-            <OnOff />
-
-            <UncontrolledAccordion titleValue = {'Menu'}/>
-
-            <UncontrolledRating />
-
-            {/*<Accordion titleValue={"Menu"} collapsed={false}/>*/}
+            <OnOff/>
+            {/*<UncontrolledAccordion titleValue = {'Menu'}/>*/}
+            {/*<UncontrolledRating />*/}
 
         </div>
     );
 }
 
-type PageTitlePropsType = {
-    title: string
-}
-
-function PageTitle(props: PageTitlePropsType) {
-    console.log('PageTitle rendering');
-    return <h1>{props.title}</h1>
-}
+// type PageTitlePropsType = {
+//     title: string
+// }
+//
+// function PageTitle(props: PageTitlePropsType) {
+//     console.log('PageTitle rendering');
+//     return <h1>{props.title}</h1>
+// }
 
 export default App;
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,19 +1,19 @@
 // import React from 'react';
 
 type AccordionPropsType = {
-  titleValue: string
-  collapsed: boolean
+    titleValue: string
+    collapsed: boolean
+    onClickAccordion: (collapsed: boolean)=>void
 }
 
 function Accordion(props: AccordionPropsType) {
-  
-    return (
-      <div>
-        <AccordionTitle title = {props.titleValue}/>
-        {!props.collapsed && <AccordionBody />}
-      </div>
-    )
 
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue} onClick={props.onClickAccordion} collapsed={!props.collapsed}/>
+            {!props.collapsed && <AccordionBody/>}
+        </div>
+    )
 }
 
 // function Accordion(props: AccordionPropsType) {
@@ -34,20 +34,22 @@ function Accordion(props: AccordionPropsType) {
 // }
 
 type AccordionTitlePropsType = {
-  title: string
+    title: string
+    onClick: (collapsed: boolean)=>void
+    collapsed: boolean
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-  console.log('AccordionTitle rendering');
-  return <h3>-- {props.title} --</h3>
+    console.log('AccordionTitle rendering');
+    return <h3 onClick={()=>{props.onClick(props.collapsed)}}>-- {props.title} --</h3>
 }
 
 function AccordionBody() {
-  console.log('AccordionBody rendering');
-  return <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
+    console.log('AccordionBody rendering');
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
     </ul>
 }
 
